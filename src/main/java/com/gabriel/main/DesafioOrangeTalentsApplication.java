d2d51ec92049b84c3b5827cc8bf5c6e806a7c4db
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-02-23 17:22:29 -0300

    first commit

---
+++ src/main/java/com/gabriel/main/DesafioOrangeTalentsApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioOrangeTalentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioOrangeTalentsApplication.class, args);
+	}
+
+}
