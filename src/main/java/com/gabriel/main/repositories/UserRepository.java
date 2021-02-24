d133042eed26ae19db4d02dfca4c13cfa9aa5800
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-02-24 16:43:45 -0300

    Created user repository

---
+++ src/main/java/com/gabriel/main/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gabriel.main.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabriel.main.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
