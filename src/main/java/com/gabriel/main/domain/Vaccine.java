26d19c4ee1fe1e03975fffdde07becd17cf72c96
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-02-24 16:31:22 -0300

    Created vaccine domain

---
+++ src/main/java/com/gabriel/main/domain/Vaccine.java
@@ -0,0 +1,75 @@
+package com.gabriel.main.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vaccine implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private LocalDate date;
+	
+	public Vaccine() {}
+
+	public Vaccine(int id, String name, LocalDate date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.date = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vaccine other = (Vaccine) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+}
