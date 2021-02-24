f898fb9ead599f38a54bcb44ded9455405e8dfb9
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-02-24 16:31:38 -0300

    Created user domain

---
+++ src/main/java/com/gabriel/main/domain/User.java
@@ -0,0 +1,121 @@
+package com.gabriel.main.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"cpf", "email"}))
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String cpf;
+	
+	@Column(nullable = false)
+	private String email;
+	private String name;
+	private LocalDate birth;
+
+	@OneToMany
+	@JoinColumn(name = "user_id", nullable = false)
+	List<Vaccine> vaccines = new ArrayList<>();
+
+	public User() {
+	}
+
+	public User(long id, String cpf, String email, String name, LocalDate birth, List<Vaccine> vaccines) {
+		super();
+		this.id = id;
+		this.cpf = cpf;
+		this.email = email;
+		this.name = name;
+		this.birth = birth;
+		this.vaccines = vaccines;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getBirth() {
+		return birth;
+	}
+
+	public void setBirth(LocalDate birth) {
+		this.birth = birth;
+	}
+
+	public List<Vaccine> getVaccines() {
+		return vaccines;
+	}
+
+	public void setVaccines(List<Vaccine> vaccines) {
+		this.vaccines = vaccines;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+}
